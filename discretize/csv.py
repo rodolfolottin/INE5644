93d2132eec5674312a217a9727b27514c060d171
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2016-11-14 00:26:15 -0200

    Implements csv.py structure

--- discretize/csv.py
+++ discretize/csv.py
@@ -1,24 +1,33 @@
 import csv
+import settings
 from attr import Attr
 
-CSV_PATH = '../csv_files/'
 
 class CSV(object):
 
-    def __init__(self, csv, list: params):
-        self.csv_file = CSV_PATH + csv
+    def __init__(self, csv, params = list(), should_return=False):
+        self.csv_file = settings.CSV_PATH + csv
         self.params = params
+        if should_return:
+            return self.read_data_from_csv()
 
     def read_data_from_csv(self):
         with open(self.csv_file, 'rt') as r_csv:
             data = csv.DictReader(r_csv, delimiter=',')
-            self.breed_info = [{param: line[param] for param in self.params}] for line in data]
-    print('Done: {}'.format(csv=self.read_data_from_csv.__name__)
+            csv_data = []
+            for line in data:
+                info = {}
+                for param in self.params:
+                    info[param] = line[param]
+                csv_data.append(info)
+        print('Done: {}'.format(csv=self.read_data_from_csv.__name__))
+        return csv_data
 
     def create_new_csv(self, csv_filename, data):
-            with open(CSV_PATH + csv_filename, 'rt') as new_csv:
-                writer = csv.DictWriter(new_csv, filenames = self.params)
-                writer.writeheader()
-                writer.writerows(data)
-        print('Done: {}. New csv file created: {}'.format(csv=self.create_new_csv.__name__, path=CSV_PATH + csv_filename)
+        final_params = [k for k in data[0]]
+        with open(settings.CSV_PATH + csv_filename, 'rt') as new_csv:
+            writer = csv.DictWriter(new_csv, filenames = final_params)
+            writer.writeheader()
+            writer.writerows(data)
+        print('Done: {}. New csv file created: {}'.format(csv=self.create_new_csv.__name__, path=settings.CSV_PATH + csv_filename))
 
